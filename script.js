b7ea50d67a442c1c66c391f9d1c6fdcde811c680
Dieg Cesare <diegocesare300491@gmail.com>
2024-08-17 21:59:34 -0300

    Adiçao da validação de tipo

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
+var select = document.getElementById("options");
+var input = document.getElementById("selectedOption");
+
 function updateInput() {
-  var select = document.getElementById("options");
-  var input = document.getElementById("selectedOption");
-  input.value = select.options[select.selectedIndex].text;
+  input.value = select.options[select.selectedIndex].value;
+}
+
+function valideType() {
+  let isValid = false;
+
+  for (let i = 0; i < select.options.length; i++) {
+    if (input.value === select.options[i].value) {
+      isValid = true;
+      break;
+    }
+  }
+
+  if (!isValid) {
+    alert("Adicione um tipo válido");
+    input.value = "";
+  }
 }
